46f9d83f549c5f3cd8690991771e3eec6e2fe982
Moa Stenqvist <moa@stonetwig.se>
2021-05-03 16:28:43 +0200

    Add header + sider menu.

--- client/src/components/ChatRooms/SiderMenu.tsx
+++ client/src/components/ChatRooms/SiderMenu.tsx
@@ -1,1 +1,65 @@
-import React from 'react';
+import { Component, CSSProperties } from 'react';
+import { Layout, Menu } from 'antd';
+import { Link, withRouter } from 'react-router-dom';
+
+const { Sider } = Layout;
+class SiderMenu extends Component {
+
+    render () {
+        return (
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={broken => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+                style={{
+                    height: '100vh',
+                    background: '#f1edea'
+                }}
+            >
+                <Menu mode="inline" style={{ background: '#f1edea' }}>
+                    <h3 style={headlineStyleOpen}>Open rooms</h3>
+                    <Menu.Item key="1">
+                        <Link to={'/'}> Room 1</Link>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Link to={'/'}>Room 2</Link>
+                    </Menu.Item>
+                    <Menu.Item key="3">
+                        <Link to={'/'}>Room 3</Link>
+                    </Menu.Item>
+                    <h3 style={headlineStylePrivate}>Private rooms</h3>
+                    <Menu.Item key="4">
+                        <Link to={'/'}> Room 1</Link>
+                    </Menu.Item>
+                    <Menu.Item key="5">
+                        <Link to={'/'}>Room 2</Link>
+                    </Menu.Item>
+                    <Menu.Item key="6">
+                        <Link to={'/'}>Room 3</Link>
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+        )          
+    }
+}
+
+export default SiderMenu;
+
+const headlineStyleOpen: CSSProperties = {
+    paddingTop: '8rem', 
+    paddingBottom: '2rem',
+    paddingLeft: '1rem',
+    fontWeight: 'bold',
+}
+
+const headlineStylePrivate: CSSProperties = {
+    paddingTop: '3rem', 
+    paddingBottom: '2rem',
+    paddingLeft: '1rem',
+    fontWeight: 'bold',
+}
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,1 +1,46 @@
-import React from 'react';
+import { Row, Col } from "antd";
+import { Header} from "antd/lib/layout/layout";
+import { Component, CSSProperties } from "react";
+import logo from '../assets/logo-white.png'; 
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+
+  render() {
+        return (
+            <Header style={layoutStyle}>
+                <Row style={{ width: '100%' }}>
+                    <Col span={24} style={colStyle}>
+                        <Link to='/'>
+                        <img src={logo} alt="logo" style={logoStyle} />
+                        </Link>
+                    </Col>
+                </Row>
+            </Header> 
+        )
+    }
+}; 
+
+export default Navbar;
+
+
+const layoutStyle: CSSProperties = {
+    width: '100%', 
+    top: '0',
+    left: '0',
+    background: '#98c3bc',
+    height: '5rem',
+    display: 'flex', 
+    alignItems:'center',
+    justifyItems:'center',
+    zIndex: 100,
+    position: 'fixed',
+  }
+
+  const colStyle: CSSProperties = {
+    width: '100%',
+  }
+
+  const logoStyle: CSSProperties = {
+   width: '10rem',
+  }
