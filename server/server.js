e9ba64f87ed47e5c36013251beb51f095a39e6d3
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2021-05-03 15:10:32 +0200

    created the basic sockets

--- server/server.js
+++ server/server.js
@@ -1,9 +1,47 @@
 const express = require('express');
+const http = require('http')
 const mongoose = require('mongoose');
+const { Server } = require("socket.io");
 
 const app = express();
 const port = 3001;
 
+const server = http.createServer(app);
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+
+    // Sends back the message to app for everyone to see
+    // When this is been fetch on the client, use it like a variable.
+    // If it is a object write variable.user or what value you would like to get
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+
+    // Send back message to everyone besides the author
+    // io.on('connection', (socket) => {
+    //     socket.on('chat message', (msg) => {
+    //     socket.broadcast.emit('chat message', msg);
+    //     });
+    // });
+
+    // Send back the message just to the author
+    // io.on('connection', (socket) => {
+    //     socket.on('chat message', (msg) => {
+    //       socket.emit('chat message', msg);
+    //     });
+    // });
+
+    // This sends a message to the client that someone has been connected or disconnected to the chatroom
+    // Use arrive to write the connect-message
+    io.emit('arrive', 'New user has been connected!');
+
+    // This sends a message to the client that someone has been disconnected from the chatroom
+    // Use leaving to write the disconnect-message
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('leaving', 'User has left the chatroom!');
+    })
+});
 
 async function run() {
     try {
